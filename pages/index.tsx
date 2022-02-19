a47aab61daaa4bab5163860d347bc98f90a8b150
Aaryaman0 <abhageri@uwaterloo.ca>
2022-02-19 16:27:38 -0500

    Added map.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import {Map} from "../component/map"
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to my fitness pal</h1>
+        <Map/>
       </main>
 
       <footer className={styles.footer}>
