165bace9973e9e513dc6960670107a2c3696f354
ElasticBottle <winstonyeo99@yahoo.com>
2022-02-20 00:33:23 -0500

    Fix type error

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { GetServerSideProps } from "next";
 import { getSession, signIn } from "next-auth/react";
 import { ROUTE_MAP } from "../interface/routes";
 
-const Home: NextPage = ({ loggedIn }: { loggedIn: boolean }) => {
+const Home = ({ loggedIn }: { loggedIn: boolean }) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-5">
       <div className="text-4xl font-bold">Gun Game but it is fitness</div>
@@ -22,7 +22,7 @@ const Home: NextPage = ({ loggedIn }: { loggedIn: boolean }) => {
       </Button>
     </div>
   );
-}
+};
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
