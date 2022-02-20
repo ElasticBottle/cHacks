8d3ccf5116bc0cc109d5943bfd6c85c3aa1523c8
ElasticBottle <winstonyeo99@yahoo.com>
2022-02-20 03:19:43 -0500

    Revert hans commit

--- atoms/data.ts
+++ atoms/data.ts
@@ -4,16 +4,19 @@ export const client = createClient({
 });
 
 export const userQuery = `
-query MyQuery {
-  calgary_hacks_location(where: {online: {_eq: true}}) {
-    online
-    location
+query MyQuery($email: String!) {
+  calgary_hacks_user(where: {email: {_eq: $email}}) {
+    email
+    image
+    reputation
+    name
+    uid
   }
 }
 `;
 
 export const userMutation = `
-mutation MyMutation($objects: [calgary_hacks_user_insert_input!] = {}) {
+mutation MyMutation($objects: [calgary_hacks_user_insert_input!]!) {
     insert_calgary_hacks_user(objects: $objects) {
       returning {
         email
